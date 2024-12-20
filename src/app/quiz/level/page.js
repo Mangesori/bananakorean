730fb65c233b47b279beb8450544e0371f056612
mangesori <vaporware08@gmail.com>
2024-12-20 16:06:25 +0800

    Update13

--- src/app/quiz/level/page.js
+++ src/app/quiz/level/page.js
@@ -1,17 +1,15 @@
-import LevelQuiz from "@/components/sections/quiz/LevelQuiz";
 import PageWrapper from "@/components/shared/wrappers/PageWrapper";
 import ThemeController from "@/components/shared/others/ThemeController";
 
 export const metadata = {
-  title: "Full-Level Quiz | Edurock - Education LMS Template",
-  description: "Full-Level Quiz | Edurock - Education LMS Template",
+  title: "Full-Level Quiz ",
+  description: "Full-Level Quiz",
 };
 
 const LevelQuizPage = () => {
   return (
     <PageWrapper>
       <main>
-        <LevelQuiz />
         <ThemeController />
       </main>
     </PageWrapper>
