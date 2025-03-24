58fb68013ced76bb97a5f9058b6e6c136225d958
mangesori <vaporware08@gmail.com>
2025-03-24 11:27:25 +0800

    Error1 컴포넌트의 이스케이프 문자 수정

--- src/components/sections/error/Error1.js
+++ src/components/sections/error/Error1.js
@@ -12,7 +12,7 @@ const Error1 = () => {
         <div className="w-full lg:max-w-770px lg:mx-auto text-center">
           <Image src={errorImage1} alt="Error" className="w-full mb-60px" placeholder="blur" />
           <h3 className="text-size-35 md:text-size-40 lg:text-size-50 leading-10 md:leading-14.5 lg:leading-20 text-blackColor dark:text-blackColor-dark font-bold">
-            Oops... It looks like you're lost!
+            Oops... It looks like you&apos;re lost!
           </h3>
           <p className="text-lg text-contentColor dark:text-contentColor-dark leading-22px">
             Oops! The page you are looking for does not exist. It might have been moved or deleted.
